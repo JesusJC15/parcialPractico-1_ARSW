bf0147c273721c4d2384d93f42a0fd4f0504d603
JesusJC15 <jesus.jauregui-c@mail.escuelaing.edu.co>
2025-09-19 07:51:53 -0500

    PiDigitsThread class created

---
+++ src/main/java/edu/eci/arsw/PiDigitsThread.java
@@ -0,0 +1,48 @@
+package edu.eci.arsw;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class PiDigitsThread extends Thread {
+    private final int start;
+    private final int end;
+    private final byte[] result;
+    private final AtomicBoolean paused;
+    private int processedDigits = 0;
+
+    public PiDigitsThread(int start, int end, byte[] result, AtomicBoolean paused) {
+        this.start = start;
+        this.end = end;
+        this.result = result;
+        this.paused = paused;
+    }
+
+    @Override
+    public void run() {
+        for (int i = start; i < end; i++) {
+            synchronized (paused) {
+                while (paused.get()) {
+                    try {
+                        paused.wait();
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                        return;
+                    }
+                }
+            }
+            result[i] = calculatePiDigit(i);
+            processedDigits++;
+
+            if (i % 1000 == 0) {
+                System.out.println("Thread " + getId() + " processed digits: " + processedDigits);
+            }
+        }
+    }
+
+    private byte calculatePiDigit(int position) {
+        return (byte) (position % 16);
+    }
+
+    public int getProcessedDigits() {
+        return processedDigits;
+    }
+}
